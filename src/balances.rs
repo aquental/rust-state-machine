b05887f76da3fd924ccb65aea961b6b1fbbf857f
Antonio Quental <aquental@users.noreply.github.com>
2024-08-06 20:54:54 -0300

    added test

--- src/balances.rs
+++ src/balances.rs
@@ -24,3 +24,20 @@ impl Pallet {
         *self.balances.get(who).unwrap_or(&0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn init_balances() {
+        /* TODO: Create a mutable variable `balances`, which is a new instance of `Pallet`. */
+        let mut balances = super::Pallet::new();
+        /* TODO: Assert that the balance of `alice` starts at zero. */
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+        /* TODO: Set the balance of `alice` to 100. */
+        balances.set_balance(&"alice".to_string(), 100);
+        /* TODO: Assert the balance of `alice` is now 100. */
+        assert_eq!(balances.balance(&"alice".to_string()), 100);
+        /* TODO: Assert the balance of `bob` has not changed and is 0. */
+        assert_eq!(balances.balance(&"bob".to_string()), 0);
+    }
+}
