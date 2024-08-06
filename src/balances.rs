107dad94b4f7bff95a7648c05567e08867b9903c
Antonio Quental <aquental@users.noreply.github.com>
2024-08-06 20:07:55 -0300

    added balance and get_balance

--- src/balances.rs
+++ src/balances.rs
@@ -12,4 +12,15 @@ impl Pallet {
             balances: BTreeMap::new(),
         }
     }
+    /// Set the balance of an account `who` to some `amount`.
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
+        /* Insert `amount` into the BTreeMap under `who`. */
+        self.balances.insert(who.to_string(), amount);
+    }
+    /// Get the balance of an account `who`.
+    /// If the account has no stored balance, we return zero.
+    pub fn balance(&self, who: &String) -> u128 {
+        /* Return the balance of `who`, returning zero if `None`. */
+        *self.balances.get(who).unwrap_or(&0)
+    }
 }
