1411fbd90ff786faf98499824875f76cdeb9fd35
Antonio Quental <antonio.quental@gmail.com>
2024-08-10 16:07:56 -0300

    Fix Pallet declaration in main

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ mod system;
 mod types {
     pub type AccountId = String;
     pub type Balance = u128;
+    pub type BlockNumber = u32;
+    pub type Nonce = u32;
 }
 // This is our main Runtime.
 // It accumulates all of the different pallets we want to use.
 #[derive(Debug)]
 pub struct Runtime {
     /// field `system` of type `system::Pallet`.
-    pub system: system::Pallet<String, u32, u32>,
+    pub system: system::Pallet<types::AccountId, types::BlockNumber, types::Nonce>,
     /// field `balances` of type `balances::Pallet`.
     pub balances: balances::Pallet<types::AccountId, types::Balance>,
 }
