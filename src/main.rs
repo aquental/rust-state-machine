e2e805958ced87974eb19b4b16b4bcd47ed942a8
Antonio Quental <antonio.quental@gmail.com>
2024-08-11 11:39:33 -0300

    Create Your Block Type

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@ mod support;
 mod system;
 
 mod types {
+    use crate::support;
+
     pub type AccountId = String;
     pub type Balance = u128;
     pub type BlockNumber = u32;
     pub type Nonce = u32;
+    pub type Extrinsic = support::Extrinsic<AccountId, crate::RuntimeCall>;
+    pub type Header = support::Header<BlockNumber>;
+    pub type Block = support::Block<Header, Extrinsic>;
+}
+
+pub enum RuntimeCall {
+    // TODO: Not implemented yet.
 }
 
 impl system::Config for Runtime {
